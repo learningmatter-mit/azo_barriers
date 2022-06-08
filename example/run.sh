55f452ea58f60d0b226e1e074d0c0ed4fa2896ca
Simon Axelrod <saxelrod@.mit.edu>
2022-06-07 20:23:31 -0400

    Everything looking good so far. Just need to export the s_t crossing in the summary

--- example/run.sh
+++ example/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 source activate barriers
-python ../barriers/utils/workflow.py
+
+# get the directory of this script
+direc="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+
+python $direc/../barriers/utils/workflow.py
